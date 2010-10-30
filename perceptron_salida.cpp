91d6a9ed444772e99617b020bd146aa422de527a
Leonardo Daniel Herbas Burgos <jhonellway@gmail.com>
2010-10-30 17:03:13 +0200

    00001

---
+++ perceptron_salida.cpp
@@ -0,0 +1,32 @@
+	
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#define NUM_ENTRADAS 2
+ 
+float entrada[NUM_ENTRADAS],
+      pesos[NUM_ENTRADAS],
+      salida;
+int i;
+ 
+using namespace std;
+ 
+int main(){
+  srand48(time(NULL));
+  for( i=0; i<NUM_ENTRADAS; i++ ){
+  pesos[i]=drand48(); //se aplican valores aleatorios a las entradas
+  }
+  for( i=0; i<NUM_ENTRADAS; i++ ){
+    cout<<"Entrada "<<i+1<<": ";
+    cin>>entrada[i]; //se pide al usuario las entradas
+  }
+  salida=0.0;      //preventivamente se inicia la salida con 0
+  for( i=0; i<NUM_ENTRADAS; i++ ){
+    salida+=(entrada[i])*(pesos[i]);//se va a la salida la
+  }                                     //suma de las entradas por los pesos
+  salida=1/(1+exp(-1*salida));       //se aplica a la salida la funcion sigmoide
+  cout<<"Salida: "<<salida<<endl;               //se imprime la salida
+  cout<<"Pesos: "<<pesos[1]<<" "<<pesos[2]<<endl; //muestra los pesos usados
+  return 0;
+}
